b1d2576c34695e6ad5441dfdf1cdfb2eed3e0a87
Diamondsiron <273699943@qq.com>
2018-04-19 10:17:20 +0800

    ruler

--- src/components/common/ruler.vue
+++ src/components/common/ruler.vue
@@ -12,7 +12,7 @@
                 </ul>
                </div>
             </div>
-            <div class="rulerPointer"></div>
+            <div class="rulerPointer" id="rulerPointer"></div>
         </div>
 		
 	</div>
@@ -31,6 +31,8 @@ export default {
     init() {
       var that = this;
       var st;
+       var pleft=0;
+       pleft = document.getElementById("rulerPointer").offsetLeft
       //滚动条实际长度
       var wrapperWidth = document.getElementById(that.rulerScroller)
         .offsetWidth;
@@ -39,30 +41,38 @@ export default {
       var leftlimit = Math.floor(maxWidth / 2 + 10);
       var scale = Math.floor(maxWidth / 2 / 10 + 1);
       // console.log(wrapperWidth,maxWidth,leftlimit);
+      var x=-1 ;
       document
         .getElementById(that.rulerScroller)
         .addEventListener("touchstart", function(e) {
           var touch = e.targetTouches[0];
           st = touch.pageX;
+           x=-1 ;
         });
 
       document
         .getElementById(that.rulerScroller)
         .addEventListener("touchmove", function(e) {
           var touch = e.targetTouches[0];
-          var x = touch.pageX;
+          var xx = touch.pageX;
+          if(x==-1){
+            	x=xx;
+            }
           var lf = document.getElementById(that.rulerScroller).offsetLeft;
           //控制速度的关键 因为挪动距离和value是对应的 修改挪动距离 速度随公式改变
-          // var nlf=(lf+x-st)/2;
-          var nlf = lf + x - st;
-          //  console.log("nlf",nlf,"lf",lf,"x",x,"st",st)
+           //var nlf=(lf+x-st)/2;
+           var distance = parseInt((x-xx)*1.0);
+          //var nlf = lf + xx - st;
+          var nlf = lf-distance;
+            console.log("nlf",nlf,"lf",lf,"x",xx,"distance",distance)
 
           if (lf - leftlimit > 0 || -wrapperWidth + maxWidth > lf) {
             return;
           } else {
             document.getElementById(that.rulerScroller).style.left = nlf + "px";
           }
-          document.getElementById(that.rulerScroller).style.left = nlf + "px";
+           x=xx;
+         // document.getElementById(that.rulerScroller).style.left = nlf + "px";
           that.value =
             Math.floor((leftlimit - nlf) / 10) > 0
               ? Math.floor((leftlimit - nlf) / 10)
@@ -73,6 +83,7 @@ export default {
       document
         .getElementById(that.rulerScroller)
         .addEventListener("touchend", function(e) {
+          console.log('end')
           var lf = document.getElementById(that.rulerScroller).offsetLeft;
           if (lf > leftlimit) {
             document.getElementById(that.rulerScroller).style.left =
@@ -85,6 +96,14 @@ export default {
             );
           }
           //var pointerVal=Math.floor((limitLeft-nDis)/(10*param.mult));
+         //  var disNo=Math.round((lf-pleft)/10);
+         //   var nDis=disNo*10+pleft;
+         //   document.getElementById(that.rulerScroller).style.left =nDis+"px"
+          //   that.value =
+          //  Math.floor((leftlimit - nDis) / 10) > 0
+          //    ? Math.floor((leftlimit - nDis) / 10)
+            //  : 0;
+
         });
       //nlf = leftlimit-10*that.vlue
       document.getElementById(that.rulerScroller).style.left =
